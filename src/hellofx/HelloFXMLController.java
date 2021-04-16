9b2c48afb45199ebc458d27b9c9a1275ac838110
Alexander Russkih <oldengremlin@gmail.com>
2021-04-16 12:12:11 +0300

    Netbeans 12.3 + JDK 14 + OpenJFX 14

---
+++ src/hellofx/HelloFXMLController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellofx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author olden
+ */
+public class HelloFXMLController implements Initializable {
+
+    @FXML
+    private Label theLabel;
+
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    private void handleClickMeButton(ActionEvent event) {
+        theLabel.setText(
+                "Hello, JavaFX "
+                + System.getProperty("java.version")
+                + "\nRunning on Java "
+                + System.getProperty("javafx.version")
+                + "."
+        );
+    }
+
+}
